aa32aba61920c9729c67c9cc6e43e50611adb9ad
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-04-20 10:13:34 -0300

    criando hash da senha

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -21,7 +22,10 @@ const UserSchema = new mongoose.Schema({
         default: Date.now,
     },
 });
-
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+});
 const User = mongoose.model('User', UserSchema);
-
 module.exports = User;
