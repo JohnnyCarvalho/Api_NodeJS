df16b359114af84c88da391713cefda367b17ff1
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-04-21 09:43:09 -0300

    corrigindo erros
    corrigindo erros de escrita e comentando o código

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,22 @@
 const express = require('express');
 
-const User = require('../models/user');//atenção para erro com a letra maiúscula no User
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
+
     try {
+        // verifica se o email já existe no banco de dados
+        if (await User.findOne({ email })) {
+            return res.status(400).send({ error: 'User already exists' });
+        }
+        // cria um novo usuário
     const user = await User.create(req.body);
+
+    // retorna o usuário criado, porém não a senha
+    user.password = undefined;
 
     return res.send({ user });
     } catch (err) {
--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema({
         require: true,
         lowercase: true,// força a converção em caixa baixa
     },
-    pssword: {
+    password: {
         type: String,
         require: true,
         select: false,// serve para quando fizer a requisição dos usuários no banco de dados a senha não vem junto no array de usuários.
@@ -22,6 +22,7 @@ const UserSchema = new mongoose.Schema({
         default: Date.now,
     },
 });
+// antes de salvar no banco de dados, a senha é criptografada
 UserSchema.pre('save', async function(next) {
     const hash = await bcrypt.hash(this.password, 10);
     this.password = hash;
